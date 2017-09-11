36a9a800af3716660e270b2f987376ddff53451e
Esteban Alexander Machuca <Esteban Alexander Machuca>
2017-09-11 20:52:16 -0300

    Agregar archivos de proyecto.

---
+++ login/Form2.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+
+    End Sub
+
+    Private Sub Form2_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        Form1.Show()
+
+    End Sub
+End Class
---
+++ login/Form3.vb
@@ -0,0 +1,31 @@
+﻿Imports MySql.Data.MySqlClient
+Public Class Form3
+
+    Private Sub Form3_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim conexion As MySqlConnection = New MySqlConnection
+        Dim comando As MySqlCommand = New MySqlCommand
+
+        comando.Connection = conexion
+
+        Try
+            conexion.ConnectionString = "Server=localhost; Database=prueba; Uid=root; Pwd=hola123;"
+            MsgBox("Conectado")
+
+        Catch ex As Exception
+            MsgBox(ex.Message)
+
+        End Try
+
+        comando.CommandText = "SELECT * FROM usuario"
+        Dim dt As DataTable = New DataTable
+        Dim da As MySqlDataAdapter = New MySqlDataAdapter(comando)
+
+        da.Fill(dt)
+        DataGridView1.DataSource = dt
+
+        conexion.Close()
+
+
+
+    End Sub
+End Class
